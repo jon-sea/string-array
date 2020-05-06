ff8d55aacd9ecd09030dbd8ff1ec7736d44ab71c
jon-sea <57441917+jon-sea@users.noreply.github.com>
2020-05-05 23:32:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let text3: number[] = []
+let mySprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . .
+    . . . . . f 5 5 5 f f . . . . .
+    . . . . f 1 5 2 5 1 6 f . . . .
+    . . . f 1 6 6 6 6 6 1 6 f . . .
+    . . . f 6 6 f f f f 6 1 f . . .
+    . . . f 6 f f d d f f 6 f . . .
+    . . f 6 f d f d d f d f 6 f . .
+    . . f 6 f d 3 d d 3 d f 6 f . .
+    . . f 6 6 f d d d d f 6 6 f . .
+    . f 6 6 f 3 f f f f 3 f 6 6 f .
+    . . f f d 3 5 3 3 5 3 d f f . .
+    . . f d d f 3 5 5 3 f d d f . .
+    . . . f f 3 3 3 3 3 3 f f . . .
+    . . . f 3 3 5 3 3 5 3 3 f . . .
+    . . . f f f f f f f f f f . . .
+    . . . . . f f . . f f . . . . .
+`, SpriteKind.Player)
+let text_list = ["Hello", "I am", "the", "Princess"]
+for (let index = 0; index <= text_list.length; index++) {
+    mySprite.say(text_list[index], 300)
+    pause(400)
+}
+text_list.reverse()
+for (let index2 = 0; index2 <= text_list.length; index2++) {
+    mySprite.say(text_list[index2], 300)
+    pause(400)
+}
